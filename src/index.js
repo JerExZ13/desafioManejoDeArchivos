e7d700a276de88552428af5b370204f28c9cdd16
Jeremias Ramos <Exzed13@hotmail.com>
2023-10-29 18:55:18 -0300

    más productos para probar

--- src/index.js
+++ src/index.js
@@ -38,14 +38,64 @@ const env = async () => {
     code: "abc1234",
     stock: 25,
   };
+  const product5 = {
+    title: "producto prueba4",
+    description: "Este es un producto prueba4",
+    price: 200,
+    thumbnail: "Sin imagen4",
+    code: "abc123asd4",
+    stock: 25,
+  };
+  const product6 = {
+    title: "producto prueba4",
+    description: "Este es un producto prueba4",
+    price: 200,
+    thumbnail: "Sin imagen4",
+    code: "abc123fas4",
+    stock: 25,
+  };
+  const product7 = {
+    title: "producto prueba4",
+    description: "Este es un producto prueba4",
+    price: 200,
+    thumbnail: "Sin imagen4",
+    code: "abc123df4",
+    stock: 25,
+  };
+  const product8 = {
+    title: "producto prueba4",
+    description: "Este es un producto prueba4",
+    price: 200,
+    thumbnail: "Sin imagen4",
+    code: "abc123ht4",
+    stock: 25,
+  };
+  const product9 = {
+    title: "producto prueba4",
+    description: "Este es un producto prueba4",
+    price: 200,
+    thumbnail: "Sin imagen4",
+    code: "abc123G4",
+    stock: 25,
+  };
+  const product10 = {
+    title: "producto prueba4",
+    description: "Este es un producto prueba4",
+    price: 200,
+    thumbnail: "Sin imagen4",
+    code: "abc123QW4",
+    stock: 25,
+  };
 
   //TESTING
 
   let addProduct = await manager.addProduct(product1);
   let addProduct1 = await manager.addProduct(product2);
   let addProduct2 = await manager.addProduct(product3);
+  let addProduct4 = await manager.addProduct(product4);
+
   //console.log(addProduct2);
-  let addProduct3 = await manager.addProduct(product4);
+  let addProduct3 = await manager.addProduct(product3);
 
   let getProducts = await manager.getProducts();
   //console.log(getProducts);
@@ -67,7 +117,14 @@ const env = async () => {
   let deleteProduct1 = await manager.deleteProduct(3);
   let deleteProduct = await manager.deleteProduct(5);
   console.log(deleteProduct1);
-  let addProduct4 = await manager.addProduct(product3);
+
+  let addProduct5 = await manager.addProduct(product5);
+  let addProduct6 = await manager.addProduct(product6);
+  let addProduct7 = await manager.addProduct(product7);
+  let addProduct8 = await manager.addProduct(product8);
+  let addProduct9 = await manager.addProduct(product9);
+  let addProduct10 = await manager.addProduct(product10);
+  let addProduct22 = await manager.addProduct(product3);
 };
 
 env();
