885b41213bf8f6344060c3676cffbc28bd9c2fa0
Jeremias Ramos <Exzed13@hotmail.com>
2023-10-29 00:31:13 -0300

    ServidorExpressCorregido

--- src/index.js
+++ src/index.js
@@ -8,38 +8,36 @@ const env = async () => {
 
   const product1 = {
     title: "producto prueba",
-    description:"Este es un producto prueba",
+    description: "Este es un producto prueba",
     price: 200,
-    thumbnail:"Sin imagen",
-    code:"abc1231",
+    thumbnail: "Sin imagen",
+    code: "abc1231",
     stock: 25,
-
   };
   const product2 = {
     title: "producto prueba2",
-    description:"Este es un producto prueba2",
+    description: "Este es un producto prueba2",
     price: 200,
-    thumbnail:"Sin imagen2",
-    code:"abc1232",
+    thumbnail: "Sin imagen2",
+    code: "abc1232",
     stock: 25,
   };
   const product3 = {
     title: "producto prueba3",
-    description:"Este es un producto prueba3",
+    description: "Este es un producto prueba3",
     price: 200,
-    thumbnail:"Sin imagen3",
-    code:"abc1233",
+    thumbnail: "Sin imagen3",
+    code: "abc1233",
     stock: 25,
   };
   const product4 = {
     title: "producto prueba4",
-    description:"Este es un producto prueba4",
+    description: "Este es un producto prueba4",
     price: 200,
-    thumbnail:"Sin imagen4",
-    code:"abc1234",
+    thumbnail: "Sin imagen4",
+    code: "abc1234",
     stock: 25,
   };
-
 
   //TESTING
 
@@ -47,7 +45,7 @@ const env = async () => {
   let addProduct1 = await manager.addProduct(product2);
   let addProduct2 = await manager.addProduct(product3);
   //console.log(addProduct2);
-  //let addProduct3 = await manager.addProduct(product4);
+  let addProduct3 = await manager.addProduct(product4);
 
   let getProducts = await manager.getProducts();
   //console.log(getProducts);
@@ -55,13 +53,21 @@ const env = async () => {
   let getProductById = await manager.getProductsById(2);
   console.log(getProductById);
 
-  let updatedProduct = await manager.updateProduct(2, "nutella", "500gr", 1200, "No image", 153, 12);
+  let updatedProduct = await manager.updateProduct(
+    2,
+    "nutella",
+    "500gr",
+    1200,
+    "No image",
+    153,
+    12
+  );
   let getProductById1 = await manager.getProductsById(2);
-  console.log(getProductById1)
+  console.log(getProductById1);
   let deleteProduct1 = await manager.deleteProduct(3);
   let deleteProduct = await manager.deleteProduct(5);
   console.log(deleteProduct1);
-
+  let addProduct4 = await manager.addProduct(product3);
 };
 
 env();
